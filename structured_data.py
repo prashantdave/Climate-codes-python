49cdc91cf2f8ae70f9767bcca42e04495f7454e6
prashantdave <prahsantdave25@gmail.com>
2013-03-15 11:56:32 +0530

    modified according to each varibale with time. Now each variable with time is stored in different file

--- structured_data.py
+++ structured_data.py
@@ -1,5 +1,5 @@
 from scipy.io import netcdf
-from numpy import size, hstack, vstack, array
+from numpy import size, hstack, vstack, array, median
 from sympy import Matrix
 import csv
 
@@ -11,39 +11,45 @@ def struct_data(file_path):
   lat = all_data.dimensions['lat']
   lon = all_data.dimensions['lon']
   mlev = all_data.dimensions['mlev']
-  count = 1
+  time = all_data.dimensions['time']
+  count = 1 
+ 
+#  for variable in list_of_variables:
+#      var_dim = len(all_data.variables[variable].dimensions)
+#      if(var_dim == 3):
+#        a = all_data.variables[variable].data
+#        b = a.mean(0).reshape(1,lat*lon).ravel()
+#        print variable, b.shape, count 
+#        count = count + 1
+#        writer.writerow(b.tolist())
+#      if (var_dim == 4):
+#        for i in range(mlev):
+#          a = all_data.variables[variable].data[:,i,:,:]
+#          b = a.mean(0).reshape(1,lat*lon).ravel()
+#          print variable, b.shape, count
+#          count = count + 1
+#          writer.writerow(b.tolist())
 
-  datawrite = open("data.txt","a")
-  writer = csv.writer(datawrite)
-  
   for variable in list_of_variables:
-    var_dim = len(all_data.variables[variable].dimensions)
-    if (var_dim >= 3):
-      if(var_dim == 3):
-        a = all_data.variables[variable].data
-        b = a.mean(0)
-        c = Matrix(b.reshape(lat*lon,1))
-        print variable, c.shape, count
-        writer.writerow(b)
-#        if count == 1:
- #           M = c
-  #      else:
-   #         M = hstack([M,c])
-    #    count = count + 1
-      if (var_dim == 4):
-        for i in range(mlev):
-          a = all_data.variables[variable].data[:,i,:,:]
-          b = a.mean(0)
+      
+      datawrite = open(variable + ".csv","w")
+      writer = csv.writer(datawrite)
+      a = all_data.variables[variable].data
+      var_dim = len(all_data.variables[variable].dimensions)
+      for i in range(31):
+        if(var_dim == 3):
+          b = a[i,:,:].reshape(1, lat*lon).ravel()
+        if (var_dim == 4):
+          b = median(a[i,:,:,:],axis=0).reshape(1, lat*lon).ravel()
+        print variable, b.shape, count 
+        count = count + 1
+        writer.writerow(b.tolist())
 
-          c = Matrix(b.reshape(lat*lon,1))
-          print variable, c.shape, count
-          writer.writerow(b)
-#        if count == 1:
-    #      if count == 1:
-     #       M = c
-      #    else:
-       #     M = hstack([M,c])
-          count = count + 1
+#        for i in range(mlev):
+#          a = all_data.variables[variable].data[:,i,:,:]
+#          b = a.mean(0).reshape(1,lat*lon).ravel()
+#          print variable, b.shape, count
+#          count = count + 1
+ #         writer.writerow(b.tolist())
+      datawrite.close()
 
-  datawrite.close()
-  return M
